e97c8d8b91b03a8314a02e51ecef5f62af6142b8
实验室207 <1619649679@qq.com>
2024-05-05 17:44:02 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'github-origin/master'.
    
    Changes to be committed:
    	new file:   .vscode/settings.json
    	deleted:    img/mint_green.jpg
    	deleted:    img/pink.jpg
    	new file:   img/qrcode.png
    	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
+// 检测屏幕宽度并进行跳转
+function redirectToMobileOrDesktop() {
+	// 获取屏幕宽度
+	const screenWidth = window.innerWidth || document.documentElement.clientWidth
+
+	// 设置阈值来判断移动设备与桌面设备的界限，这里假设小于等于 768px 为移动设备
+	const threshold = 768
+
+	// 如果屏幕宽度小于等于阈值，跳转至移动端网站
+	if (screenWidth <= threshold) {
+		window.location.href = 'https://m-zyzx.netlify.app'
+	}
+	// else {
+	// 如果屏幕宽度大于阈值，跳转至桌面端网站
+	// window.location.href = '链接到桌面端网站的URL'
+	// }
+}
+
+// 在页面加载完成后调用函数
+window.onload = redirectToMobileOrDesktop
+
+
+
+
+
+
+
+
 let img = document.querySelector('.banner img') // 获取图片元素
 let imageIndex = 1 // 图片索引，从第二张开始
 
